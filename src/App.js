e96a7c8e34a8c7493bf28f1f0c1f751c1453671f
edinhassagic <edinhassagic@gmail.com>
2021-10-23 02:28:27 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
+import React from "react";
+import "./App.css"
+import Header from "./components/Header";
+import Hangie from "./components/Hangie";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import { useState, useEffect } from "react";
+
+const words = ['edin', 'edna', 'maja', 'marko']
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+
+
+
+
+
 
 
 function App() {
+  
+  
+  
+  const [playable, setPlayable] = useState(true)
+   const [correctLetters, setCorrectLetters] = useState([])
+   const [wrongLetters, setWrongLetters] = useState([])
+
+ useEffect (() => {
+const handleKeyDown = event => {
+  const {key, keyCode} = event;
+  if ( playable && keyCode >= 65 && keyCode <= 90) {
+    const letter = key.toLowerCase();
+  
+if (selectedWord.includes(letter)) {
+  if (!correctLetters.includes(letter)) {
+    setCorrectLetters(currentLeters => [...currentLeters, letter]);
+  } else {
+    // showNotification()
+  } 
+} else {
+    if (!wrongLetters.includes(letter)) {
+      setWrongLetters(currentLeters => [...currentLeters, letter])
+    } else {
+      // showNotification()
+    }
+  }
+}
+
+
+}
+window.addEventListener('keydown', handleKeyDown);
+
+return () => window.removeEventListener('keydown', handleKeyDown);
+
+ }, [correctLetters, wrongLetters, playable]);
+
+
+
+
+
+
   return (
-    <div className="App">
-      <h1>Hangman</h1>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Hangie wrongLetters={wrongLetters}  />
+        <WrongLetters wrongLetters={wrongLetters}/>
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+
+    </>
+
   );
 }
 
